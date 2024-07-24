c36440d2b5716db69d7ca01ba8c64695a5590ed4
Hans Poreda <hansporeda@icloud.com>
2024-07-24 10:23:31 +0200

    kotlin version updated

---
+++ android/app/src/main/kotlin/com/drinke/lindenhofpark/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.drinke.lindenhofpark
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/lindenhofpark/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lindenhofpark
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
